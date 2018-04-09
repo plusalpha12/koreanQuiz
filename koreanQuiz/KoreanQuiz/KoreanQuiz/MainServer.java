08365edff908cb35854ea17adcbff5f5666bba04
plusalpha12 <33535076+plusalpha12@users.noreply.github.com>
2018-04-09 22:28:17 +0900

    db

--- koreanQuiz/KoreanQuiz/KoreanQuiz/MainServer.java
+++ koreanQuiz/KoreanQuiz/KoreanQuiz/MainServer.java
@@ -7,6 +7,7 @@ import java.net.Socket;
 import java.net.SocketException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Scanner;
  
 public class MainServer {
     
@@ -15,6 +16,9 @@ public class MainServer {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
     	
+    	int exit;
+    	Scanner scan = new Scanner(System.in);
+    	
         try(ServerSocket server = new ServerSocket(6060)) {
         	
         	while (true) {
@@ -22,6 +26,11 @@ public class MainServer {
                 System.out.println(getTime()+"서버 준비");
                 System.out.println(getTime()+"클라이언트 대기");
                 
+                exit = scan.nextInt();
+                
+                if(exit == 1) {
+                    System.exit(0);
+                }
         		Socket client = server.accept();
                 Server_thread ST = new Server_thread(client);
                 ST.run();
