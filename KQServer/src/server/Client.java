9f94277d2c31701299d1da72ec110c821b9ffd26
plusalpha12 <33535076+plusalpha12@users.noreply.github.com>
2018-04-30 22:15:43 +0900

    test

---
+++ KQServer/src/server/Client.java
@@ -0,0 +1,31 @@
+package server;
+
+public class Client{
+	
+	private InitialGameRoom room = null;
+	private int roomnum = 0;
+	private String userid = null;
+	private String username = null;
+	private int Score = 0;
+
+	public Client() {}
+
+	public Client(String userid, String username)
+	{
+		this.userid = userid;
+		this.username = username;
+	}
+
+	public void enterRoom(InitialGameRoom room) {
+		room.enterUser(this);
+		this.room = room;
+	}
+
+	public void exitRoom(InitialGameRoom room) {
+		this.room = null;
+	}
+
+	public String getUserid() {return userid;}
+
+	public String getUsername() {return username;}
+}
