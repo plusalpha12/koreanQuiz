350e5f40641501653c736f004eed84dfdc337810
plusalpha12 <33535076+plusalpha12@users.noreply.github.com>
2018-05-31 12:04:04 +0900

    서버 최적화

--- KQServer/src/server/Game.java
+++ KQServer/src/server/Game.java
@@ -3,45 +3,45 @@ package server;
 import java.util.Random;
 
 public class Game {
-	
+
 	static public String getRandomString(int length) {
 		int level = 0;
 		int min = 0;
 		int rand = 0;
 		StringBuffer buffer = new StringBuffer();
 		Random random = new Random();
-		
+
 		String chars[] = "ぁ,い,ぇ,ぉ,け,げ,さ,し,じ,ず,せ,ぜ,そ,ぞ,あ,え,こ,ざ,す".split(",");
-		int lev[] = 	{7,13,10,12,9,10,8,5,6,15,24,24,28,16,33,33,35,33,35};
-		
+		int lev[] = 	{7,13,10,12,9,10,8,5,2,15,24,24,28,16,36,36,36,36,36};
+
 		if(length == 2) {
 			level = 35;
 			while (buffer.length() < 2) {
 				rand = random.nextInt(lev.length);
 				min = lev[rand];
 				level -= min;
-				buffer.append(chars[rand]);
 				if(level < 0) {
-					level = 35;
-					buffer.delete(0, length);
-				}
+					level += min;
+					continue;
+				}else
+					buffer.append(chars[rand]);
 			}
-			
+
 		}else if(length == 3) {
 			level = 45;
 			while (buffer.length() < 3) {
 				rand = random.nextInt(lev.length);
 				min = lev[rand];
 				level -= min;
-				buffer.append(chars[rand]);
 				if(level < 0) {
-					level = 45;
-					buffer.delete(0, length);
-				}
+					level += min;
+					continue;
+				}else
+					buffer.append(chars[rand]);
 			}
-			
+
 		}
-		
+
 		return buffer.toString();
 	}
 }
